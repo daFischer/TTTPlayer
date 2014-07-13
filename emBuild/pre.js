9df8d57b4e7840fcaf427414280a32d3644b5777
daFischer <johannes.w.fischer@web.de>
2014-07-13 17:14:24 +0200

    Audio now uses Html5 audio tag

---
+++ emBuild/pre.js
@@ -0,0 +1,4 @@
+Module['preRun'] = function() {
+		FS.createPreloadedFile("/TTT/", "Diskrete_Strukturen_2013_11_21.ttt", "http://www.theether.heliohost.org/TTT/TTT/Diskrete_Strukturen_2013_11_21_a/Diskrete_Strukturen_2013_11_21.ttt", true, false)
+		FS.createPreloadedFile("/TTT/", "Diskrete_Strukturen_2013_11_21.mp3", "http://www.theether.heliohost.org/TTT/TTT/Diskrete_Strukturen_2013_11_21_a/Diskrete_Strukturen_2013_11_21.mp3", true, false)
+};
